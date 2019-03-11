15e6239a566d752c84c44256bd8bad5db29cf0c4
devacc <devacc201@gmail.com>
2019-03-11 14:40:39 -0400

    Gson

--- src/main/java/org/deloitte/td/utils/AemTarget.java
+++ src/main/java/org/deloitte/td/utils/AemTarget.java
@@ -1,16 +1,21 @@
 package org.deloitte.td.utils;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
+import java.net.*;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.X509Certificate;
 import java.util.*;
 
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import org.deloitte.td.dto.MigrationDto;
+
+import javax.net.ssl.*;
 
 public class AemTarget {
 
@@ -24,7 +29,7 @@ public class AemTarget {
 
   public void run() {
     dataMigration = loadMigrationFile("/Users/jimmyhernandez/Documents/projects/td/config-files/source2.csv");
-    taxonomy = loadTaxonomy("/Users/jimmyhernandez/Documents/projects/td/config-files/taxonomy.csv");
+    //taxonomy = loadTaxonomy("/Users/jimmyhernandez/Documents/projects/td/config-files/taxonomy.csv");
     otherFiles = mappingFile("/Users/jimmyhernandez/Documents/projects/td/config-files/others.csv");
 
     //System.out.println(otherFiles.get("79101"));
@@ -35,32 +40,111 @@ public class AemTarget {
       String aemPath = null;
       if ( m.getContainers().equals("ORPHAN") ) {
         aemPath = otherFiles.get(m.getId());
+//        System.out.println(m.getId() + " " + aemPath);
       } else if ( m.getContainers().contains("$Containers:") ) {
         if (m.getContainers().contains("MBNA")) {
           aemPath = mbna(m.getContainers());
         } else if ( m.getContainers().contains("|") && !m.getContainers().contains("MBNA") ) {
           aemPath = multipleContainer(m.getContainers());
-        } else if ( !m.getContainers().contains("MBNA") && !m.getContainers().equals("$Containers:TD:TD (Can)") && !m.getContainers().contains("|") ) {
+        } else if ( !m.getContainers().contains("MBNA") && !m.getContainers().contains("|") ) {
           aemPath = notMbna(m.getContainers());
         }
       }
-//        String rcontainer = m.getContainers().isEmpty() ? otherFiles.get(m.getId()) : m.getContainers();
-      if (aemPath != null) {
+
+//      if (aemPath != null) {
         results.add(aemPath+"\t"+m.getId());
-      }
+//      }
 
     }
 
-    Path file = Paths.get("/Users/jimmyhernandez/Documents/projects/td/config-files/the-file-name2.csv");
+    Path file = Paths.get("/Users/jimmyhernandez/Documents/projects/td/config-files/path-result.csv");
     try {
       Files.write(file, results, Charset.forName("UTF-8"));
     } catch (IOException e) {
       e.printStackTrace();
     }
 
+    try {
 
-    List<String> records = loadContainerFile("/Users/jimmyhernandez/Documents/projects/td/config-files/containers-example.csv");
-    containersReview(records);
+      Authenticator.setDefault (new Authenticator() {
+        protected PasswordAuthentication getPasswordAuthentication() {
+          return new PasswordAuthentication("jimmyhernandez", "P@ssw0rd123!".toCharArray());
+        }
+      });
+
+
+      // Create a trust manager that does not validate certificate chains
+      TrustManager[] trustAllCerts = new TrustManager[] {new X509TrustManager() {
+        public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+          return null;
+        }
+        public void checkClientTrusted(X509Certificate[] certs, String authType) {
+        }
+        public void checkServerTrusted(X509Certificate[] certs, String authType) {
+        }
+      }
+      };
+
+      // Install the all-trusting trust manager
+      SSLContext sc = null;
+      try {
+        sc = SSLContext.getInstance("SSL");
+        sc.init(null, trustAllCerts, new java.security.SecureRandom());
+        HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+      } catch (NoSuchAlgorithmException e) {
+        e.printStackTrace();
+      } catch (KeyManagementException e) {
+        e.printStackTrace();
+      }
+
+
+      // Create all-trusting host name verifier
+      HostnameVerifier allHostsValid = new HostnameVerifier() {
+        public boolean verify(String hostname, SSLSession session) {
+          return true;
+        }
+      };
+
+      // Install the all-trusting host verifier
+      HttpsURLConnection.setDefaultHostnameVerifier(allHostsValid);
+
+      URL url = new URL("https://13.88.236.91/content/dam/Canada/small-business-banking/5369-1018_SBB_Advice_Series-A_Banner-OCT19_TDCT_WB_A.jpg/jcr:content/metadata.json");
+      HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+
+      conn.setRequestMethod("GET");
+      int code = conn.getResponseCode();
+
+      System.out.println(code);
+
+      StringBuilder result = new StringBuilder();
+      BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+
+      String line;
+
+      while((line = rd.readLine()) != null) {
+        result.append(line);
+      }
+
+      rd.close();
+
+
+      JsonParser jsonParser = new JsonParser();
+      JsonObject jo = (JsonObject)jsonParser.parse(result.toString());
+
+      System.out.println("dam:sha1" + jo.get("dam:sha1"));
+
+//      if (resultSha1.equals(jo.get("dam:sha1"))) {
+//        System.out.println("true");
+//      } else {
+//        System.out.println("false");
+//      }
+
+    } catch (MalformedURLException e) {
+      e.printStackTrace();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+
   }
 
   public void containersReview(List<String> records) {
@@ -91,7 +175,7 @@ public class AemTarget {
       }
     }
 
-    Path file = Paths.get("/Users/jimmyhernandez/Documents/projects/td/config-files/the-file-name.csv");
+    Path file = Paths.get("/Users/jimmyhernandez/Documents/projects/td/config-files/filtered.csv");
     try {
       Files.write(file, results, Charset.forName("UTF-8"));
     } catch (IOException e) {
