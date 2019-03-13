431f919ac6983fc7992d53bec369927cda74f5f3
devacc <devacc201@gmail.com>
2019-03-13 15:00:33 -0400

    Canto assets

--- src/main/java/org/deloitte/td/Main.java
+++ src/main/java/org/deloitte/td/Main.java
@@ -1,9 +1,8 @@
 package org.deloitte.td;
 
-import org.deloitte.td.dto.MigrationDto;
+
 import org.deloitte.td.utils.AemTarget;
 
-import java.util.List;
 
 public class Main {
   public static void main(String[] args) {
--- src/main/java/org/deloitte/td/utils/AemTarget.java
+++ src/main/java/org/deloitte/td/utils/AemTarget.java
@@ -23,6 +23,7 @@ public class AemTarget {
   private Map<String,String> paths;
   private Map<String,String> orphans;
   private Map<String,String> props;
+  private Map<String,String> cantoInfo;
   private List<MigrationDto> dataMigration;
 
   public AemTarget() {
@@ -30,73 +31,89 @@ public class AemTarget {
   }
 
   public void run() {
-
-    String assetsPath = props.get("assetPath");
+    String HEADER_RESULT = "Canto Id\tFile name\tAEM Migration Path\tAEM URL\tFilesize\tSha1 Code\tAEM Found";
+    String HEADER_NOT_FOUND_FILES = "Canto Id\tCanto path\tRenamed file\tFile name\tAsset exist";
 
     dataMigration = loadMigrationFile(props.get("containers"));
     paths = loadTaxonomy(props.get("paths"));
     orphans = mappingFile(props.get("orphans"));
+    cantoInfo = mappingFile(props.get("cantoInfo"));
 
-    List<String> results = new ArrayList<>();
     List<String> foundAssets = new ArrayList<>();
     List<String> notFoundAssets = new ArrayList<>();
+    List<String> cantoResults = new ArrayList<>();
 
-    for(MigrationDto m: dataMigration){
-      String aemPath = null;
-      if ( m.getContainers().equals("ORPHAN") ) {
-        aemPath = orphans.get(m.getId());
-      } else if ( m.getContainers().contains("$Containers:") ) {
-        if (m.getContainers().contains("MBNA")) {
-          aemPath = mbna(m.getContainers());
-        } else if ( m.getContainers().contains("|") && !m.getContainers().contains("MBNA") ) {
-          aemPath = multipleContainer(m.getContainers());
-        } else if ( !m.getContainers().contains("MBNA") && !m.getContainers().contains("|") ) {
-          aemPath = notMbna(m.getContainers());
-        }
-      }
+    cantoResults.add(HEADER_RESULT);
+    notFoundAssets.add(HEADER_NOT_FOUND_FILES);
 
-      if (aemPath != null) {
-        results.add(aemPath+"\t"+m.getId());
-      }
+    int i = 0;
+    int max = 6;
 
-      File asset = this.getFile(assetsPath + "/" +m.getId());
+    for (MigrationDto dto : dataMigration) {
+      // Get Canto path based on Id
+      String assetFile = "NA";
+      String cantoPath = cantoInfo.get(dto.getId());
+      File asset = new File(cantoPath);
+      if ( asset.exists() && asset.isDirectory() ) {
+        // Get Asset based on filename - search Assets renamed folder
+        assetFile = props.get("cantoAssetsRenamedFolder") +"/"+dto.getFileName();
+        asset = new File(assetFile);
 
-      if (asset.exists() && !asset.isDirectory()) {
-        String shaCode = generateSha(asset);
-        foundAssets.add(m.getId()+","+asset.length()+","+m.getFileName()+","+shaCode+","+aemPath);
-      } else {
-        notFoundAssets.add(m.getId()+",Asset - Not Found,"+m.getFileName());
-      }
+        if ( asset.length() > 0 ) {
+          // Get AEM path based on Container value
+          String aemPath = getAemPath(dto);
+          // Generate the sha code for the file
+          String sha1Code = generateSha(asset);
 
-      String assetFile = props.get("aemServer") + aemPath + "/" + m.getFileName();
-      HttpURLConnection assetConn = assetAemValidation(assetFile,props.get("aemUserName"),props.get("aemPassword"));
+          String assetAemFile = props.get("aemServer") + aemPath + "/" + dto.getFileName();
+          // Http Get to validated if the asset exist on AEM
+          HttpURLConnection assetConn = assetAemValidation(assetAemFile,props.get("aemUserName"),props.get("aemPassword"));
+          String aemAssetFound = "Not Found";
 
-      try {
-        if(assetConn.getResponseCode() == 200) {
-          System.out.println("Exist");
-          JsonObject md = aemMetadata(assetConn);
+          try {
+            if(assetConn.getResponseCode() == 200) {
+  //                JsonObject md = aemMetadata(assetConn);
+              System.out.println("Found");
+              aemAssetFound = "Found";
+            }
+          } catch (IOException e) {
+            e.printStackTrace();
+          }
+
+          cantoResults.add(dto.getId()+"\t"+dto.getFileName()+"\t"+aemPath+"\t"+assetAemFile+"\t"+asset.length()+"\t"+sha1Code+"\t"+aemAssetFound);
         } else {
-          System.out.println("Doesn\'t Exist");
+          notFoundAssets.add(dto.getId()+"\t"+cantoPath+"\t"+assetFile+"\t"+dto.getFileName()+"\t"+asset.exists());
         }
-      } catch (IOException e) {
-        e.printStackTrace();
-      }
 
+      } else {
+        notFoundAssets.add(dto.getId()+"\t"+cantoPath+"\t"+assetFile+"\t"+dto.getFileName()+"\t"+asset.exists());
+      }
+      i++;
     }
 
-    wrCsvFile(foundAssets,props.get("foundFile"));
+
+    wrCsvFile(cantoResults,props.get("cantoResults"));
     wrCsvFile(notFoundAssets,props.get("notFoundFile"));
 
-    Path file = Paths.get(props.get("resultsFile"));
-    try {
-      Files.write(file, results, Charset.forName("UTF-8"));
-    } catch (IOException e) {
-      e.printStackTrace();
-    }
-
-
-
   }
+
+  public String getAemPath(MigrationDto m) {
+    String aemPath = null;
+    if ( m.getContainers().equals("ORPHAN") ) {
+      aemPath = orphans.get(m.getId());
+    } else if ( m.getContainers().contains("$Containers:") ) {
+      if (m.getContainers().contains("MBNA")) {
+        aemPath = mbna(m.getContainers());
+      } else if ( m.getContainers().contains("|") && !m.getContainers().contains("MBNA") ) {
+        aemPath = multipleContainer(m.getContainers());
+      } else if ( !m.getContainers().contains("MBNA") && !m.getContainers().contains("|") ) {
+        aemPath = notMbna(m.getContainers());
+      }
+    }
+    return aemPath;
+  }
+
+
 
   public String findAemPath(String container) {
     String cantoPath = "";
@@ -367,12 +384,13 @@ public class AemTarget {
       result.put("foundFile",prop.getProperty("foundFile"));
       result.put("notFoundFile",prop.getProperty("notFoundFile"));
 
-      result.put("rootPath",prop.getProperty("rootPath"));
-      result.put("assetPath",prop.getProperty("assetPath"));
-
       result.put("containers",prop.getProperty("containers"));
       result.put("paths",prop.getProperty("paths"));
       result.put("orphans",prop.getProperty("orphans"));
+
+      result.put("cantoInfo",prop.getProperty("cantoInfo"));
+      result.put("cantoResults",prop.getProperty("cantoResults"));
+      result.put("cantoAssetsRenamedFolder",prop.getProperty("cantoAssetsRenamedFolder"));
 
       inputStream.close();
 
